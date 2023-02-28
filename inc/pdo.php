92405677003424cd66d6c7194302100986821b75
Allan76370 <grout.allan76@gmail.com>
2023-02-28 16:29:37 +0100

    boucle limite de 30 + index

--- inc/pdo.php
+++ inc/pdo.php
@@ -1,10 +1,16 @@
 <?php
+
+define('HOST', 'localhost');
+$dbName = 'netflix';
+$userDbName = 'allan';
+$userDbPassword = 'allan';
+
 try {
-    $conn = new PDO('mysql:host=localhost;dbname=netflix',"allan","allan", [
+    $conn = new PDO('mysql:host=' . HOST . ';dbname=' . $dbName, $userDbName, $userDbPassword, array(
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
-    ]);
+    ));
 } catch (PDOException $erreur) {
     echo 'Erreur de connexion: ' . $erreur->getMessage();
 }
--- index.php
+++ index.php
@@ -1,2 +1,30 @@
 <?php
 // Lister les films
+require './inc/fonctions.php';
+require './inc/pdo.php';
+
+// $sqlRequest = "SELECT * FROM movies_full WHERE title like '%vadrouille%'";
+$sqlRequest = "SELECT * FROM movies_full WHERE title LIMIT 0,30";
+
+
+$resultat = $conn->prepare($sqlRequest);
+$resultat->execute();
+$films = $resultat->fetchAll();
+
+//dd($films);
+
+if (count($films) == 0) {
+    echo 'aucun résultat';
+} else {
+    echo '<div>';
+    $i = 1;
+    foreach ($films as $key => $value) {
+        echo 'Film n°' . $i . '<br>';
+        echo 'Titre: ' . $value['title'] . '<br>';
+        echo 'Genre: ' . $value['genres'] . '<br>';
+        echo 'Acteurs: ' . $value['cast'] . '<br>';
+        echo '<hr>';
+        $i++;
+    }
+    echo '</div>';
+}
